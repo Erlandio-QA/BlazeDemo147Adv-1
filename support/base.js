df70cc8e36a3c46603135853a6b6553023c736d8
Erlandio-QA <erlandiosilva1028@gmail.com>
2026-05-27 19:17:08 -0300

    27-5/26 19:00

--- support/base.js
+++ support/base.js
@@ -1,5 +1,9 @@
 const { setWorldConstructor} = require('@cucumber/cucumber') // adicionamos World para importar a classe
 const { chromium, firefox, webkit } = require('@playwright/test')
+const HomePage = require('../pages/home.page')
+const ReservePage = require('../pages/reserve.page' )
+const PurchasePage = require('../pages/purchase.page')
+const ConfirmationPage = require('../pages/confirmation.page' )
 
 // aqui tbm
 class Base{
@@ -8,8 +12,14 @@ class Base{
     const browser_name = process.env.BROWSER || 'chromium'
 
     this.browser = await browsers[browser_name].launch({ headless: process.env.HEADLESS !== 'false' })
-    this.context = await this.browser.newContext()
+    this.context = await this.browser.newContext({ baseURL: 'https://www.blazedemo.com' })
     this.page = await this.context.newPage()
+
+    this.homePage = new HomePage(this.page)
+    this.reservePage = new ReservePage(this.page)
+    this.purchasePage = new PurchasePage(this.page) 
+    this.confirmationPage = new ConfirmationPage(this.page)
+
   }
 
   async close_browser() {
